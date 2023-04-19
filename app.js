9efceb4faa4af71a3ab1e08abb8557d9c019efa8
Tonia <anthoniachiamaka28@gmail.com>
2023-04-19 02:36:44 +0100

    Added Comments

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
     var buttons = document.querySelectorAll(".btn");
     var storeImages = document.querySelectorAll(".store-item");
 
+    //loop through the button indidvidually to know the actually button clicked
+
 buttons.forEach((button) =>{
     button.addEventListener("click", (e)=>{
         e.preventDefault()
-        var filter = e.target.dataset.filter
-        console.log(filter)
 
+        // get the filter name using the dataset.filter
+        var filter = e.target.dataset.filter
+        
+
+    // loop through the images and use the filter to ascertain which image has the same filter name as button clicked
         storeImages.forEach((item) => {
             if (filter === "all") {
                 item.style.display ="block"
